ecb12846d77c79229b342085aa991a79c3a65941
Jared Breeden <jared.breeden@gmail.com>
2015-02-22 21:00:05 -0500

    Adding sample image

--- samples/sinatra/browser.rb
+++ samples/sinatra/browser.rb
@@ -1,3 +1,2 @@
 RbChrome::Browser.window_title = "TODO MVC - as performed by Sinatra"
-puts "CACHE PATH: #{File.dirname(__FILE__)}/cache"
 RbChrome::Browser.cache_path = "#{File.dirname(__FILE__)}/cache"

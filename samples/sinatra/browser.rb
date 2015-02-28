2f761c228aad3f697274917a5677fbebdc1a4f24
Jared Breeden <jared.breeden@gmail.com>
2015-02-28 13:04:39 -0500

    Removing some cache from git. Sample updated

--- samples/sinatra/browser.rb
+++ samples/sinatra/browser.rb
@@ -1,3 +1,3 @@
-RbChrome::Browser.window_title = "TODO MVC - as performed by Sinatra"
+RbChrome::Browser.window_title = "TODO MVC - As performed by Sinatra"
 RbChrome::Browser.cache_path = "#{File.dirname(__FILE__)}/cache"
 RbChrome::Browser.remote_debugging_port = 8888

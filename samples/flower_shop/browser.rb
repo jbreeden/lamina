97992aece3fb7ec472b12a7f4fcd470fa7593f90
Jared Breeden <jared.breeden@gmail.com>
2015-03-15 09:49:18 -0400

    Updated flower shop sample

--- samples/flower_shop/browser.rb
+++
@@ -1,4 +0,0 @@
-RbChrome::Browser.window_title = "Flower Shop"
-RbChrome::Browser.disable_page_titles
-RbChrome::Browser.cache_path = "#{File.dirname(__FILE__)}/cache"
-RbChrome::Browser.remote_debugging_port = 8888
--- samples/flower_shop/server.rb
+++ samples/flower_shop/server.rb
@@ -2,7 +2,6 @@ require 'sinatra'
 
 enable :run
 disable :logging
-set :port, $RB_CHROME_OPTIONS[:port]
 
 get '/' do
   send_file "#{settings.public_folder}/index.html"

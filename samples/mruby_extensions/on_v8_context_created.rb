613370712978ed01624b9c415b15a3407a3b7115
Jared Breeden <jared.breeden@gmail.com>
2015-03-16 22:13:31 -0400

    Updating extensions sample

--- samples/mruby_extensions/on_v8_context_created.rb
+++ samples/mruby_extensions/on_v8_context_created.rb
@@ -20,6 +20,33 @@ begin
       V8.window[:alert].call(V8.window, [arg1])
     end
 
+    V8.window[:rbTypeCheck] = V8.create_function "rbTypeCheck" do |arguments|
+      arg = arguments[:shift].call(arguments, [])
+      if arg.is_undefined
+        V8.create_string 'is_undefined'
+      elsif arg.is_null
+        V8.create_string 'is_null'
+      elsif arg.is_bool
+        V8.create_string 'is_bool'
+      elsif arg.is_int
+        V8.create_string 'is_int'
+      elsif arg.is_uint
+        V8.create_string 'is_uint'
+      elsif arg.is_double
+        V8.create_string 'is_double'
+      elsif arg.is_date
+        V8.create_string 'is_date'
+      elsif arg.is_string
+        V8.create_string 'is_string'
+      elsif arg.is_object
+        V8.create_string 'is_object'
+      elsif arg.is_array
+        V8.create_string 'is_array'
+      elsif arg.is_function
+        V8.create_string 'is_function'
+      end
+    end
+
     throwing_on_purpose = true
     V8.create_int 'test'
   rescue TypeError => ex

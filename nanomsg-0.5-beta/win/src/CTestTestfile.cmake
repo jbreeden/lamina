581f1a98980c447b95839706e30ad478eb2fdc9d
Jared Breeden <jared.breeden@gmail.com>
2015-03-21 18:03:39 -0400

    Initial commit of binaries-win

--- nanomsg-0.5-beta/win/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/projects/rb-chrome/nanomsg-0.5-beta/src
-# Build directory: C:/projects/rb-chrome/nanomsg-0.5-beta/win/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- samples/flower_shop/on_app_started.rb
+++
@@ -1,5 +0,0 @@
-Lamina.load_server "ruby", "./server.rb"
-Lamina.set_window_title "Flower Shop"
-Lamina.use_page_titles false
-Lamina.set_cache_path "cache"
-Lamina.set_remote_debugging_port = 8888

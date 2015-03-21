55771857f429fdfa12a33466ae04cef4624dce35
Jared Breeden <jared.breeden@gmail.com>
2015-03-21 17:16:55 -0400

    Removing unneeded files

--- project-template/browser.rb
+++
@@ -1,6 +0,0 @@
-Lamina.load_server "ruby", "./server.rb"
-
-Lamina.set_window_title "Lamina Template"
-Lamina.use_page_titles false
-Lamina.set_cache_path "cache"
-Lamina.set_remote_debugging_port 8888
